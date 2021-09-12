7db9584a348c51dc25d9e01329698546fc78ff39
ydzydzydz <mail@zhuangzhuang.ml>
2021-09-12 17:05:45 +0800

    update

--- release.sh
+++ release.sh
@@ -4,13 +4,15 @@ name=ssl-check
 version=$(bash $name -v | awk '{print $2}')
 release=packages
 [ ! -d $release ] && mkdir -p $release
+_date=$(date +"%s")
+
 case $1 in 
 	-h|--help|help|?)
 		echo "Usage: $(basename $0) [ubuntu|centos]"
 		;;
 	centos)
 	        sed -i "s/Version.*/Version:\t$version/" build/CentOS/$name.spec
-		[ -d /root/rpmbuild ] && rm -rf /root/rpmbuild
+		[ -d /root/rpmbuild ] && mv /root/rpmbuild /root/rpmbuild_bak_$_date
 		mkdir -p /root/rpmbuild/{SOURCES,SPECS}
 		cp build/CentOS/$name.spec /root/rpmbuild/SPECS
 	       	cp $name /root/rpmbuild/SOURCES
@@ -19,15 +21,17 @@ case $1 in
 		cd -
 		cp /root/rpmbuild/RPMS/*/*.rpm $release
 		rm -rf /root/rpmbuild
+		[ -d /root/rpmbuild_bak_$_date ] && mv /root/rpmbuild_bak_$_date /root/rpmbuild
 		;;
 	ubuntu)
 	        sed "s/Version.*/Version:\t$version/" build/Ubuntu/control
-		[ -d /root/debbuild ] && rm -rf /root/debbuild
+		[ -d /root/debbuild ] && mv /root/debbuild /root/debbuild_bak_$_date
 		mkdir -p /root/debbuild/{DEBIAN,/usr/local/bin}
 		cp build/Ubuntu/control /root/debbuild/DEBIAN
 		cp $name /root/debbuild/usr/local/bin
 	        dpkg -b /root/debbuild $release/$name-$version-ubuntu.x86_64.deb
 		rm -rf /root/debbuild
+		[ -d /root/debbuild_bak_$_date ] && mv /root/debbuild_bak_$_date /root/debbuild
 		;;
 	*)
 		$0 centos
